e5316351971f48a943edc28d190af2496a01f365
Amanda House <aeh247@cornell.edu>
2017-09-10 19:52:14 -0400

    Updated model and script for watermark pages.

--- branch_generator/generate.py
+++ branch_generator/generate.py
@@ -1,8 +1,11 @@
 # Author: Amanda House (aeh247@cornell.edu)
 
-import csv, glob, os
+import csv, glob, os, sys
 
-os.makedirs('out')
+try:
+    os.makedirs('out')
+except OSError:
+    pass
 
 with open('template_question.html') as infile:
     template_question = infile.read()
@@ -18,8 +21,55 @@ with open('template_base.html') as infile, open('out/temp_watermark.html', 'w') 
         line = line.replace('replace_main', template_watermark)
         outfile.write(line)
 
-print 'Folder:'
-os.chdir(raw_input())
+def clean(prefix):
+    os.remove(prefix + 'out/temp_question.html')
+    os.remove(prefix + 'out/temp_watermark.html')
+
+try:
+    os.chdir(sys.argv[1])
+except:
+    print 'Error. Need to provide a valid directory as input.'
+    clean()
+
+try:
+    os.chdir('watermarks')
+except:
+    print 'Error. Input directory must have a `watermarks` folder..'
+    clean('../')
+
+for file in glob.glob('*.csv'):
+
+    with open(file, 'rb') as infile:
+        form = list(csv.reader(infile))
+
+    watermark_name = form[0][1]
+    short_name     = form[1][1]
+    watermark_text = form[2][1]
+    back_link      = form[3][1]
+    branch_name    = form[4][1]
+    restart_link   = form[5][1]
+
+    replacements = {
+        'replace_branch_name': branch_name,
+        'replace_back_link': back_link,
+        'replace_restart_link': restart_link,
+        'replace_watermark_name': watermark_name,
+        'replace_watermark_text': watermark_text,
+        'replace_image': short_name
+    }
+
+    with open('../../out/temp_watermark.html') as infile, open('../../out/' + short_name + '.html', 'w') as outfile:
+        for line in infile:
+            for src, target in replacements.iteritems():
+                line = line.replace(src, target)
+            outfile.write(line)
+
+os.chdir('../')
+try:
+    os.chdir('questions')
+except:
+    print 'Error. Input directory must have a `questions` folder..'
+    clean('../')
 
 for file in glob.glob('*.csv'):
 
@@ -50,30 +100,10 @@ for file in glob.glob('*.csv'):
         'replace_image': short_name
     }
 
-    with open('../out/temp_question.html') as infile, open('../out/' + short_name + '.html', 'w') as outfile:
+    with open('../../out/temp_question.html') as infile, open('../../out/' + short_name + '.html', 'w') as outfile:
         for line in infile:
             for src, target in replacements.iteritems():
                 line = line.replace(src, target)
             outfile.write(line)
 
-for file in glob.glob('*.txt'):
-
-    with open(file) as infile:
-        lines = infile.read().splitlines()
-    watermark = lines[0]
-    description = lines[1]
-
-    replacements = {
-        'replace_watermark': watermark,
-        'replace_text': description,
-        'replace_image': watermark
-    }
-
-    with open('../out/temp_watermark.html') as infile, open('../out/' + watermark + '.html', 'w') as outfile:
-        for line in infile:
-            for src, target in replacements.iteritems():
-                line = line.replace(src, target)
-            outfile.write(line)
-
-os.remove('../out/temp_question.html')
-os.remove('../out/temp_watermark.html')
+clean('../../')
