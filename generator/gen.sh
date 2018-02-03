1ae87e163c7c8b521b86ee9e9c6c35639483a704
Craig Riecke <cr396@cornell.edu>
2018-02-03 16:39:38 -0500

    Add data files for Basel Crosier

--- generator/gen.sh
+++ generator/gen.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-python generate.py single_headed_eagle
-cp out/* ../../rembrandtwireproject.github.io/single_headed_eagle
+# rm out/*
+# python generate.py single_headed_eagle
+# cp out/* ../../rembrandtwireproject.github.io/single_headed_eagle
+
+rm out/*
+python generate.py basel_crosier
+#cp out/* ../../rembrandtwireproject.github.io/basel_crosier
